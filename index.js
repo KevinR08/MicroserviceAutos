7d90e73497afc985ff666ad46f704afd5c2927c6
KEVIN MAURICIO RIVADENEIRA BEJARANO <kevin.rivadeneira@epn.edu.ec>
2024-03-03 12:17:49 -0500

    Port changed from 3000 to 3001

--- index.js
+++ index.js
@@ -182,7 +182,7 @@ app.put('/api/update/car/:idCar', isAuthenticated, async (req, res) => {
 
 
   // Inicio del servidor
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Servidor iniciado en el puerto ${PORT}`)
 })
